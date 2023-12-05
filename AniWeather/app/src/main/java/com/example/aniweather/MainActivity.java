be2a970607cc37d5071d4ade89d99635a54a8d30
Guillaume Vern <guillaume.vern@outlook.com>
2023-12-05 12:06:34 +0100

    Création projet Android studio

---
+++ AniWeather/app/src/main/java/com/example/aniweather/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.aniweather;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+
+
+}
